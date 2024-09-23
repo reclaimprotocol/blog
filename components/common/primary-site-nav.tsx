f854ef09251ffcdc483fb686006afd07d0b76b52
Abdul Rashid <ar1242112@gmail.com>
2024-09-23 19:05:47 +0530

    chore: update navbar links

--- components/common/primary-site-nav.tsx
+++ components/common/primary-site-nav.tsx
@@ -8,7 +8,7 @@ const PrimarySiteNav = () => {
       <div className="flex flex-wrap justify-between md:flex-nowrap md:gap-14">
         <div className="order-1 hidden w-full flex-col items-center justify-start md:order-none md:flex md:w-auto md:flex-1 md:flex-row md:justify-end">
           <Link
-            href={"/"}
+            href={"https://reclaimprotocol.org"}
             className="px-5 py-2 text-base font-medium text-gray-600 hover:text-blue-500 dark:text-gray-400"
           >
             Home
@@ -38,7 +38,7 @@ const PrimarySiteNav = () => {
         </div>
         <div className="order-2 hidden w-full flex-col items-center justify-start md:order-none md:flex md:w-auto md:flex-1 md:flex-row">
           <Link
-            href={"/posts"}
+            href={"/blog/posts"}
             className="px-5 py-2 text-base font-medium text-gray-600 hover:text-blue-500 dark:text-gray-400"
           >
             Blogs
